f481724b16d0210c28873a51d47e1c4a72ff25b3
Dai chou <tomy0705c@gmail.com>
2020-04-05 21:39:38 +0800

    add config from gem5

--- config/se.py
+++ config/se.py
@@ -41,6 +41,7 @@
 # Simple test script
 #
 # "m5 test.py"
+
 from __future__ import print_function
 from __future__ import absolute_import
 
@@ -52,8 +53,10 @@ import m5
 from m5.defines import buildEnv
 from m5.objects import *
 from m5.util import addToPath, fatal, warn
-from m5.objects import BaseCache
 
+addToPath('/')
+
+from ruby import Ruby
 
 from common import Options
 from common import Simulation
@@ -64,8 +67,6 @@ from common import MemConfig
 from common.FileSystemConfig import config_filesystem
 from common.Caches import *
 from common.cpu2000 import *
-
-
 
 def get_processes(options):
     """Interprets provided options and returns a list of processes"""
@@ -118,36 +119,12 @@ def get_processes(options):
         return multiprocesses, 1
 
 
-
-
-class L1Cache(BaseCache):
-    assoc = 4
-    response_latency = 2
-    tag_latency = 2
-    mshrs = 4 # miss status handling register,to achieve noblocking cache
-    tgts_per_mshr = 20
-class L1ICache(L1Cache):
-    size = '64kB'
-    def connectCPU(self, cpu):
-        self.cpu_side = cpu.icache_port
-    def connectBus(self, bus):
-        self.mem_side = bus.slave
-    def connectMemSideBus(self, bus):
-        self.mem_side = bus.slave
-
-class L1DCache(L1Cache):
-    size = '64kB'
-    def connectCPU(self, cpu):
-        self.cpu_side = cpu.dcache_port
-    def connectBus(self, bus):
-        self.mem_side = bus.slave
-    def connectMemSideBus(self, bus):
-        self.mem_side = bus.slave
-
 parser = optparse.OptionParser()
 Options.addCommonOptions(parser)
 Options.addSEOptions(parser)
 
+if '--ruby' in sys.argv:
+    Ruby.define_options(parser)
 
 (options, args) = parser.parse_args()
 
@@ -158,20 +135,46 @@ if args:
 multiprocesses = []
 numThreads = 1
 
-options.cpu_type = "DerivO3CPU"
-multiprocesses, numThreads = get_processes(options)
+if options.bench:
+    apps = options.bench.split("-")
+    if len(apps) != options.num_cpus:
+        print("number of benchmarks not equal to set num_cpus!")
+        sys.exit(1)
+
+    for app in apps:
+        try:
+            if buildEnv['TARGET_ISA'] == 'alpha':
+                exec("workload = %s('alpha', 'tru64', '%s')" % (
+                        app, options.spec_input))
+            elif buildEnv['TARGET_ISA'] == 'arm':
+                exec("workload = %s('arm_%s', 'linux', '%s')" % (
+                        app, options.arm_iset, options.spec_input))
+            else:
+                exec("workload = %s(buildEnv['TARGET_ISA', 'linux', '%s')" % (
+                        app, options.spec_input))
+            multiprocesses.append(workload.makeProcess())
+        except:
+            print("Unable to find workload for %s: %s" %
+                  (buildEnv['TARGET_ISA'], app),
+                  file=sys.stderr)
+            sys.exit(1)
+elif options.cmd:
+    multiprocesses, numThreads = get_processes(options)
+else:
+    print("No workload specified. Exiting!\n", file=sys.stderr)
+    sys.exit(1)
+
+
 (CPUClass, test_mem_mode, FutureClass) = Simulation.setCPUClass(options)
 CPUClass.numThreads = numThreads
 
-# np = options.num_cpus
-np = 1
-options.cacheline_size = 256
-options.mem_size = '512MB'
-options.sys_clock = '1MHz'
-options.cpu_clock = '1MHz'
+# Check -- do not allow SMT with multiple CPUs
+if options.smt and options.num_cpus > 1:
+    fatal("You cannot use SMT with multiple CPUs!")
 
-system = System(cpu = CPUClass(cpu_id = 0),
-                mem_mode = 'timing',
+np = options.num_cpus
+system = System(cpu = [CPUClass(cpu_id=i) for i in range(np)],
+                mem_mode = test_mem_mode,
                 mem_ranges = [AddrRange(options.mem_size)],
                 cache_line_size = options.cacheline_size)
 
@@ -203,6 +206,15 @@ if options.elastic_trace_en:
 for cpu in system.cpu:
     cpu.clk_domain = system.cpu_clk_domain
 
+if ObjectList.is_kvm_cpu(CPUClass) or ObjectList.is_kvm_cpu(FutureClass):
+    if buildEnv['TARGET_ISA'] == 'x86':
+        system.kvm_vm = KvmVM()
+        for process in multiprocesses:
+            process.useArchPT = True
+            process.kvmInSE = True
+    else:
+        fatal("KvmCPU can only be used in SE mode with x86")
+
 # Sanity check
 if options.simpoint_profile:
     if not ObjectList.is_noncaching_cpu(CPUClass):
@@ -210,66 +222,61 @@ if options.simpoint_profile:
     if np > 1:
         fatal("SimPoint generation not supported with more than one CPUs")
 
-system.cpu[0].workload = multiprocesses
+for i in range(np):
+    if options.smt:
+        system.cpu[i].workload = multiprocesses
+    elif len(multiprocesses) == 1:
+        system.cpu[i].workload = multiprocesses[0]
+    else:
+        system.cpu[i].workload = multiprocesses[i]
 
-if options.simpoint_profile:
-    system.cpu[0].addSimPointProbe(options.simpoint_interval)
+    if options.simpoint_profile:
+        system.cpu[i].addSimPointProbe(options.simpoint_interval)
 
-if options.checker:
-    system.cpu[0].addCheckerCpu()
+    if options.checker:
+        system.cpu[i].addCheckerCpu()
 
-if options.bp_type:
-    bpClass = ObjectList.bp_list.get(options.bp_type)
-    system.cpu[0].branchPred = bpClass()
+    if options.bp_type:
+        bpClass = ObjectList.bp_list.get(options.bp_type)
+        system.cpu[i].branchPred = bpClass()
 
-if options.indirect_bp_type:
-    indirectBPClass = \
-        ObjectList.indirect_bp_list.get(options.indirect_bp_type)
-    system.cpu[0].branchPred.indirectBranchPred = indirectBPClass()
+    if options.indirect_bp_type:
+        indirectBPClass = \
+            ObjectList.indirect_bp_list.get(options.indirect_bp_type)
+        system.cpu[i].branchPred.indirectBranchPred = indirectBPClass()
 
-system.cpu[0].createThreads()
+    system.cpu[i].createThreads()
 
-l1icache = L1ICache()
-l1dcache = L1DCache()
+if options.ruby:
+    Ruby.create_system(options, False, system)
+    assert(options.num_cpus == len(system.ruby._cpu_ports))
 
-#CacheConfig.config_cache(options, system)
-#MemConfig.config_mem(options, system)
-config_filesystem(system, options)
-MemClass = Simulation.setMemClass(options)
-system.cpu[0].addPrivateSplitL1Caches(l1icache,l1dcache)
-#system.cpu[0].icache = L1ICache()
-#system.cpu[0].dcache = L1DCache()
-#system.cpu[0].icache.connectCPU(system.cpu[0])
-#system.cpu[0].dcache.connectBus(system.cpu[0])
-#system.cpu[0].icache.connectMemSideBus(system.membus)
-#system.cpu[0].dcache.connectMemSideBus(system.membus)
+    system.ruby.clk_domain = SrcClockDomain(clock = options.ruby_clock,
+                                        voltage_domain = system.voltage_domain)
+    for i in range(np):
+        ruby_port = system.ruby._cpu_ports[i]
 
-system.membus = CoherentXBar()
-# Connect the system up to the membus
-system.system_port = system.membus.slave
+        # Create the interrupt controller and connect its ports to Ruby
+        # Note that the interrupt controller is always present but only
+        # in x86 does it have message ports that need to be connected
+        system.cpu[i].createInterruptController()
 
-# Create a DDR3 memory controller
-system.mem_ctrl = DDR3_1600_8x8()
-system.mem_ctrl.range = system.mem_ranges[0]
-system.mem_ctrl.port = system.membus.master
-system.cpu[0].connectAllPorts(system.membus)
-#system.cpu[0].createInterruptController()
-#system.cpu[0].interrupts[0].pio = system.membus.master
-#system.cpu[0].interrupts[0].int_master = system.membus.slave
-#system.cpu[0].interrupts[0].int_slave = system.membus.master
-#system.system_port = system.membus.slave
-system.cpu[0].createInterruptController()
-
-system.cpu[0].fetchWidth=2
-system.cpu[0].issueWidth=2
-system.cpu[0].dispatchWidth=2
-system.cpu[0].decodeWidth=2
-system.cpu[0].commitWidth=2
-system.cpu[0].renameWidth=2
-system.cpu[0].wbWidth=4
-system.cpu[0].squashWidth=2
-system.cpu[0].commitToIEWDelay=2
+        # Connect the cpu's cache ports to Ruby
+        system.cpu[i].icache_port = ruby_port.slave
+        system.cpu[i].dcache_port = ruby_port.slave
+        if buildEnv['TARGET_ISA'] == 'x86':
+            system.cpu[i].interrupts[0].pio = ruby_port.master
+            system.cpu[i].interrupts[0].int_master = ruby_port.slave
+            system.cpu[i].interrupts[0].int_slave = ruby_port.master
+            system.cpu[i].itb.walker.port = ruby_port.slave
+            system.cpu[i].dtb.walker.port = ruby_port.slave
+else:
+    MemClass = Simulation.setMemClass(options)
+    system.membus = SystemXBar()
+    system.system_port = system.membus.slave
+    CacheConfig.config_cache(options, system)
+    MemConfig.config_mem(options, system)
+    config_filesystem(system, options)
 
 root = Root(full_system = False, system = system)
 Simulation.run(options, root, system, FutureClass)
-
